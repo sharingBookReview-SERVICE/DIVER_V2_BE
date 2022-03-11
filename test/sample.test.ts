056ec10cec7b03633abb47410bed182d22a8fdde
SeungBin Kim <seungbin0508@gmail.com>
2022-03-11 12:39:04 +0900

    Add sample test with supertest

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,4 +1,11 @@
-it('should add 1 and 2', () => {
-	const k: string = 'test'
-	expect(1 + 2).toBe(3)
+import request from 'supertest'
+import app from '../src/app'
+
+it('should add 1 and 2', async () => {
+	const book_id = '9788937460777'
+	await request(app)
+		.get(`/books/${book_id}`)
+		.expect(200, {
+			_id: book_id
+		})
 })
