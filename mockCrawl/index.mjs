503bae7a773a04b6050f999ac776b055821a7ce8
SeungBin Kim <seungbin0508@gmail.com>
2022-03-25 12:06:17 +0900

    Change global code into a function

--- mockCrawl/index.mjs
+++ mockCrawl/index.mjs
@@ -12,24 +12,31 @@ import fs from 'fs/promises'
 const bidArr = [8902711, 5128976]
 const result = []
 
-const URI = 'https://book.naver.com/bookdb/book_detail.naver?bid='
-for (const bid of bidArr) {
-	const link = URI + bid
-	const { data } = await axios.get(URI + bid)
-	const $ = cheerio.load(data)
+/**
+ * Crawl book data and randomly generate review_count property. Saves result as JSON file.
+ * @param bidArr {bid[]}
+ * @returns {Promise<void>}
+ */
+async function fakeBooks (bidArr) {
+	const URI = 'https://book.naver.com/bookdb/book_detail.naver?bid='
+	for (const bid of bidArr) {
+		const link = URI + bid
+		const { data } = await axios.get(URI + bid)
+		const $ = cheerio.load(data)
 
-	// 크롤링하는 속성
-	const title = $('div.book_info > h2 > a').children().remove().end().text().trim() //https://runkit.com/fffact/59293292f48c2a00125cc277
-	const image = $('div.thumb_type img').attr('src')
-	const author = $('div.book_info_inner > div:nth-child(2) > a:nth-child(2)').text()
-	const price = $('div.book_info_inner div.lowest .price').text()
-	const discount = $('div.book_info_inner div.lowest span.discount').text().match(/\d+/)?.[0] / 100 || 0
-	const pubdate = $('div.book_info_inner > div:nth-child(2)').children().remove().end().text().trim().replaceAll('.', '-')
+		// 크롤링하는 속성
+		const title = $('div.book_info > h2 > a').children().remove().end().text().trim() //https://runkit.com/fffact/59293292f48c2a00125cc277
+		const image = $('div.thumb_type img').attr('src')
+		const author = $('div.book_info_inner > div:nth-child(2) > a:nth-child(2)').text()
+		const price = $('div.book_info_inner div.lowest .price').text()
+		const discount = $('div.book_info_inner div.lowest span.discount').text().match(/\d+/)?.[0] / 100 || 0
+		const pubdate = $('div.book_info_inner > div:nth-child(2)').children().remove().end().text().trim().replaceAll('.', '-')
 
-	// 랜덤으로 생성하는 속성
-	const review_count = Math.floor(Math.random() * 100)
+		// 랜덤으로 생성하는 속성
+		const review_count = Math.floor(Math.random() * 100)
 
-	result.push({title, link, image, author, price, discount, pubdate, review_count})
+		result.push({title, link, image, author, price, discount, pubdate, review_count})
+	}
+
+	await fs.writeFile('./crawl.json', JSON.stringify(result))
 }
-
-await fs.writeFile('./crawl.json', JSON.stringify(result))
