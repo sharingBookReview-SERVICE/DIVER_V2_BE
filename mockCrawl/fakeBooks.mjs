23d8b1fd410aa6feec2b78db08131d6958f9a2a3
SeungBin Kim <seungbin0508@gmail.com>
2022-03-25 13:47:20 +0900

    Add _id property using bson-objectid

--- mockCrawl/fakeBooks.mjs
+++ mockCrawl/fakeBooks.mjs
@@ -1,15 +1,13 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
 import fs from 'fs/promises'
+import ObjectID from 'bson-objectid'
+
 /**
  * Book's ID from Naver book search (https://book.naver.com)
  * @typedef {number} bid
  */
 
-/**
- * @type {bid[]}
- */
-const bidArr = [8902711, 5128976]
 /**
  * Crawl book data and randomly generate review_count property. Saves result as JSON file.
  * @param bidArr {bid[]}
@@ -33,9 +31,10 @@ export default async function fakeBooks (bidArr) {
 		const pubdate = $('div.book_info_inner > div:nth-child(2)').children().remove().end().text().trim().replaceAll('.', '-')
 
 		// 랜덤으로 생성하는 속성
+		const _id = ObjectID()
 		const review_count = Math.floor(Math.random() * 100)
 
-		result.push({title, link, image, author, price, discount, pubdate, review_count})
+		result.push({_id, title, link, image, author, price, discount, pubdate, review_count})
 	}
 
 	await fs.writeFile('./mockCrawl/books.json', JSON.stringify(result))
